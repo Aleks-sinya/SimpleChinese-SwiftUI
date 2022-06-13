2666ac0fdea6881afb61f5b236839bb86f71563c
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-06-13 11:49:19 +0700

    Initial Commit

---
+++ SimpleChinese–SwiftUI/SimpleChinese_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleChinese_SwiftUIApp.swift
+//  SimpleChinese–SwiftUI
+//
+//  Created by Алексей Синяговский on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimpleChinese_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
